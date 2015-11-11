de4c554d8644b65ef777de9bc6b39639dbd7a51e
Stephen J Day <stephen.day@docker.com>
2015-11-10 22:23:56 -0800

    fs/p9p/new: add naive, concurrent request handing in server
    Signed-off-by: Stephen J Day <stephen.day@docker.com>

--- server.go
+++ server.go
@@ -52,120 +52,160 @@ type server struct {
 	closed  chan struct{}
 }
 
-type activeTag struct {
-	ctx       context.Context
-	request   *Fcall
-	cancel    context.CancelFunc
-	responded bool // true, if some response was sent (Response or Rflush/Rerror)
+// activeRequest includes information about the active request.
+type activeRequest struct {
+	ctx     context.Context
+	request *Fcall
+	cancel  context.CancelFunc
 }
 
 func (s *server) run() {
-	tags := map[Tag]*activeTag{} // active requests
+	tags := map[Tag]*activeRequest{} // active requests
+
+	requests := make(chan *Fcall) // sync, read-limited
+	responses := make(chan *Fcall)
+	completed := make(chan *Fcall, 1)
+
+	// read loop
+	go func() {
+		for {
+			req := new(Fcall)
+			if err := s.ch.ReadFcall(s.ctx, req); err != nil {
+				if err, ok := err.(net.Error); ok {
+					if err.Timeout() || err.Temporary() {
+						continue
+					}
+				}
+
+				log.Println("server: error reading fcall", err)
+				return
+			}
+
+			select {
+			case requests <- req:
+			case <-s.ctx.Done():
+				log.Println("server: context done")
+				return
+			case <-s.closed:
+				log.Println("server: shutdown")
+				return
+			}
+		}
+	}()
+
+	// write loop
+	go func() {
+		for {
+			select {
+			case resp := <-responses:
+				if err := s.ch.WriteFcall(s.ctx, resp); err != nil {
+					log.Println("server: error writing fcall:", err)
+				}
+			case <-s.ctx.Done():
+				log.Println("server: context done")
+				return
+			case <-s.closed:
+				log.Println("server: shutdown")
+				return
+			}
+		}
+	}()
 
 	log.Println("server.run()")
 	for {
+		log.Println("server:", "wait")
 		select {
+		case req := <-requests:
+			log.Println("request", req)
+			if _, ok := tags[req.Tag]; ok {
+				select {
+				case responses <- newErrorFcall(req.Tag, ErrDuptag):
+					// Send to responses, bypass tag management.
+				case <-s.ctx.Done():
+					return
+				case <-s.closed:
+					return
+				}
+				continue
+			}
+
+			switch msg := req.Message.(type) {
+			case MessageTflush:
+				log.Println("server: flushing message", msg.Oldtag)
+
+				var resp *Fcall
+				// check if we have actually know about the requested flush
+				active, ok := tags[msg.Oldtag]
+				if ok {
+					active.cancel() // cancel the context of oldtag
+					resp = newFcall(req.Tag, MessageRflush{})
+				} else {
+					resp = newErrorFcall(req.Tag, ErrUnknownTag)
+				}
+
+				select {
+				case responses <- resp:
+					// bypass tag management in completed.
+				case <-s.ctx.Done():
+					return
+				case <-s.closed:
+					return
+				}
+			default:
+				// Allows us to session handlers to cancel processing of the fcall
+				// through context.
+				ctx, cancel := context.WithCancel(s.ctx)
+
+				// The contents of these instances are only writable in the main
+				// server loop. The value of tag will not change.
+				tags[req.Tag] = &activeRequest{
+					ctx:     ctx,
+					request: req,
+					cancel:  cancel,
+				}
+
+				go func(ctx context.Context, req *Fcall) {
+					var resp *Fcall
+					msg, err := s.handler.Handle(ctx, req.Message)
+					if err != nil {
+						// all handler errors are forwarded as protocol errors.
+						resp = newErrorFcall(req.Tag, err)
+					} else {
+						resp = newFcall(req.Tag, msg)
+					}
+
+					select {
+					case completed <- resp:
+					case <-ctx.Done():
+						return
+					case <-s.closed:
+						return
+					}
+				}(ctx, req)
+			}
+		case resp := <-completed:
+			log.Println("completed", resp)
+			// only responses that flip the tag state traverse this section.
+			active, ok := tags[resp.Tag]
+			if !ok {
+				panic("BUG: unbalanced tag")
+			}
+
+			select {
+			case responses <- resp:
+			case <-active.ctx.Done():
+				// the context was canceled for some reason, perhaps timeout or
+				// due to a flush call. We treat this as a condition where a
+				// response should not be sent.
+				log.Println("canceled", resp, active.ctx.Err())
+			}
+			delete(tags, resp.Tag)
 		case <-s.ctx.Done():
 			log.Println("server: context done")
 			return
 		case <-s.closed:
 			log.Println("server: shutdown")
-		default:
-		}
-
-		// BUG(stevvooe): This server blocks on reads, calls to handlers and
-		// write, effectively single tracking fcalls through a target
-		// dispatcher. There is no reason we couldn't parallelize these
-		// requests out to the dispatcher to get massive performance
-		// improvements.
-
-		log.Println("server:", "wait")
-		req := new(Fcall)
-		if err := s.ch.ReadFcall(s.ctx, req); err != nil {
-			if err, ok := err.(net.Error); ok {
-				if err.Timeout() || err.Temporary() {
-					continue
-				}
-			}
-
-			log.Println("server: error reading fcall", err)
 			return
 		}
-
-		if _, ok := tags[req.Tag]; ok {
-			resp := newErrorFcall(req.Tag, ErrDuptag)
-			if err := s.ch.WriteFcall(s.ctx, resp); err != nil {
-				log.Printf("error sending duplicate tag response: %v", err)
-			}
-			continue
-		}
-
-		// handle flush calls. The tag never makes it into active from here.
-		if mf, ok := req.Message.(MessageTflush); ok {
-			log.Println("flushing message", mf.Oldtag)
-
-			// check if we have actually know about the requested flush
-			active, ok := tags[mf.Oldtag]
-			if ok {
-				active.cancel() // cancel the context
-
-				resp := newFcall(MessageRflush{})
-				resp.Tag = req.Tag
-				if err := s.ch.WriteFcall(s.ctx, resp); err != nil {
-					log.Printf("error responding to flush: %v", err)
-				}
-				active.responded = true
-			} else {
-				resp := newErrorFcall(req.Tag, ErrUnknownTag)
-				if err := s.ch.WriteFcall(s.ctx, resp); err != nil {
-					log.Printf("error responding to flush: %v", err)
-				}
-			}
-
-			continue
-		}
-
-		// TODO(stevvooe): Add handler timeout here, as well, if we desire.
-
-		// Allows us to signal handlers to cancel processing of the fcall
-		// through context.
-		ctx, cancel := context.WithCancel(s.ctx)
-
-		tags[req.Tag] = &activeTag{
-			ctx:     ctx,
-			request: req,
-			cancel:  cancel,
-		}
-
-		var resp *Fcall
-		msg, err := s.handler.Handle(ctx, req.Message)
-		if err != nil {
-			// all handler errors are forwarded as protocol errors.
-			resp = newErrorFcall(req.Tag, err)
-		} else {
-			resp = newFcall(msg)
-		}
-		resp.Tag = req.Tag
-
-		if err := ctx.Err(); err != nil {
-			// NOTE(stevvooe): We aren't really getting our moneys worth for
-			// how this is being handled. We really need to dispatch each
-			// request handler to a separate thread.
-
-			// the context was canceled for some reason, perhaps timeout or
-			// due to a flush call. We treat this as a condition where a
-			// response should not be sent.
-			log.Println("context error:", err)
-			continue
-		}
-
-		if !tags[req.Tag].responded {
-			if err := s.ch.WriteFcall(ctx, resp); err != nil {
-				log.Println("server: error writing fcall:", err)
-				continue
-			}
-		}
-
-		delete(tags, req.Tag)
 	}
 }
