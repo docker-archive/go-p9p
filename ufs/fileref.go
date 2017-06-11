b1b8898e7cb788d8274eb8d4c9cca1fe380f505a
Jeff Lindsay <progrium@gmail.com>
2017-06-11 18:02:49 -0500

    ufs: added mostly working unix filesystem server session implementation
    Signed-off-by: Jeff Lindsay <progrium@gmail.com>

---
+++ ufs/fileref.go
@@ -0,0 +1,33 @@
+package ufs
+
+import (
+	"os"
+	"sync"
+
+	p9p "github.com/docker/go-p9p"
+)
+
+type FileRef struct {
+	sync.Mutex
+	Path    string
+	Info    p9p.Dir
+	File    *os.File
+	Readdir *p9p.Readdir
+}
+
+func (f *FileRef) Stat() error {
+	f.Lock()
+	defer f.Unlock()
+
+	info, err := os.Lstat(f.Path)
+	if err != nil {
+		return err
+	}
+
+	f.Info = dirFromInfo(info)
+	return nil
+}
+
+func (f *FileRef) IsDir() bool {
+	return f.Info.Mode&p9p.DMDIR > 0
+}
